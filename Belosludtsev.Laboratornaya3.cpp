c86fa07e823d444998d74bb46114f11b77cc0c00
YuriiBelosludtsev <71659366+YuriiBelosludtsev@users.noreply.github.com>
2021-11-01 15:44:52 +0500

    Add files via upload

---
+++ Belosludtsev.Laboratornaya3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define PI 3.14159265
+double d;
+float summan, summae, x, y, a=(PI/5), b=(9*PI/5), c, k=10.0, e, step=(a+b)/k, n=40.0, f=1;
+int main ()
+{
+ for (x=a; x<=b; x+=step)
+{
+      summan=0;
+      summae=0;
+      f=1;
+
+       for (c=1; c<=n; c++)
+      {
+            summan += (cos(c*x))/c;
+            
+      }
+      do
+      {
+            e=0.0001;
+            d = ((cos(f*x))/f);
+            f++;
+            summae +=d;
+      }
+      while (d<e);
+        y= (-1)*log(abs(2*sin(0.5*x)));
+        printf("X=%.2f SN=%f SE=%f Y=%f\n",x,summan,summae,y);
+ }
+    return 0;
+}
